f347bbbfcc30d5f338e9d51b3b2ef68587f797f9
Philippe Turner <philippeturner@gmail.com>
2021-04-14 21:59:02 -0400

    Fix

--- rainbow/rainbow.py
+++ rainbow/rainbow.py
@@ -4,7 +4,7 @@ import random
 from copy import deepcopy
 from collections import deque
 
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Layer, Activation
 from keras.models import Model
 from keras.losses import MeanSquaredError
 from keras.optimizers import Adam
@@ -27,6 +27,47 @@ class DefaultMemory():
     def size(self):
         return len(self.memory)
 
+class NoisyDense(Layer):
+    def __init__(self, units, input_dim, std_init=0.5, use_bias=True):
+        super().__init__()
+        self.units = units
+        self.std_init = std_init
+        self.use_bias = use_bias
+        self.reset_noise(input_dim)
+
+        mu_range = 1 / np.sqrt(input_dim)
+        mu_initializer = tf.random_uniform_initializer(-mu_range, mu_range)
+        sigma_initializer = tf.constant_initializer(self.std_init / np.sqrt(self.units))
+
+        self.weight_mu = tf.Variable(initial_value=mu_initializer(shape=(input_dim, units), dtype='float32'),
+                                     trainable=True)
+
+        self.weight_sigma = tf.Variable(initial_value=sigma_initializer(shape=(input_dim, units), dtype='float32'),
+                                        trainable=True)
+        if self.use_bias:
+            self.bias_mu = tf.Variable(initial_value=mu_initializer(shape=(units,), dtype='float32'),
+                                        trainable=True)
+
+            self.bias_sigma = tf.Variable(initial_value=sigma_initializer(shape=(units,), dtype='float32'),
+                                        trainable=True)
+
+    def call(self, inputs):
+        self.kernel = self.weight_mu + self.weight_sigma * self.weights_eps
+        if self.use_bias:
+            self.bias = self.bias_mu + self.bias_sigma * self.bias_eps
+        return tf.matmul(inputs, self.kernel) + self.bias
+
+    def _scale_noise(self, dim):
+        noise = tf.random.normal([dim])
+        return tf.sign(noise) * tf.sqrt(tf.abs(noise))
+
+    def reset_noise(self, input_shape):
+        eps_in = self._scale_noise(input_shape)
+        eps_out = self._scale_noise(self.units)
+        self.weights_eps = tf.multiply(tf.expand_dims(eps_in, 1), eps_out)
+        if self.use_bias:
+            self.bias_eps = eps_out
+
 class Rainbow():
     def __init__(self,
             env,
@@ -41,7 +82,6 @@ class Rainbow():
             noisy_net_theta=0.5, noisy_net_enabled=False,
             prioritization_w=0.5, prioritized_memory_enabled=False,
             atoms=51, v_min=-10, v_max=10, categorical_enabled=False):
-        super().__init__(Rainbow, self)
         self.env = env
         self.memory = memory
         if self.memory is None:
@@ -83,7 +123,7 @@ class Rainbow():
 
     def update_model(self):
         q_weights = self.q_model.get_weights()
-        if self.ddqn_enable:
+        if self.dd_enabled:
             q_target_weights = self.q_target_model.get_weights()
             for i in range(len(target_weights)):
                 q_target_weights[i] = self.tau * q_weights[i] + \
@@ -97,7 +137,11 @@ class Rainbow():
         x = Dense(24, activation='relu')(inputs)
         x = Dense(64, activation='relu')(x)
         x = Dense(24, activation='relu')(x)
-        action = Dense(output_shape, activation='linear')(x)
+        if self.noisy_net_enabled:
+            action = NoisyDense(output_shape, x.shape[1])
+            action = Activation('linear')
+        else:    
+            action = Dense(output_shape, activation='linear')(x)
         return Model(inputs=inputs, outputs=action)
 
     def action(self, state, testing=False):
@@ -204,4 +248,7 @@ import gym
 
 env = gym.make("CartPole-v1")
 rain = Rainbow(env, memory=DefaultMemory())
+
+
+# n_step > 1 activate multistep
 rain.train(render=True, n_step=1)
